d49e128322197d509e04b87e87a5bdb51c3c952e
Fernando Isnaldo Silva de Faria <fernando-i@outlook.com>
2026-09-07 05:36:12 -0300

    Update GeradordeSenhas.java

--- GeradordeSenhas.java
+++ GeradordeSenhas.java
@@ -15,8 +15,8 @@ public class GeradordeSenhas {
                              O parâmetro [variação de caracteres] é opcional.
                              
                              [variação de caracteres] sem parâmetro ou com valor 0 imprime caracteres ASCII na faixa de 33 até 126.
-                             [variação de caracteres] com valor 1 imprime só números decimais.
-                             [variação de caracteres] com valor 2 imprime só letras e números.
+                             [variação de caracteres] com valor 1 imprime números decimais.
+                             [variação de caracteres] com valor 2 imprime letras e números.
                              [variação de caracteres] com valor 3 imprime números hexadecimais.
                                
                              Exemplos de uso:
