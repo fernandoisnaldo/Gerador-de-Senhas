5c86bf0551a285dd9e648dee00bcd5a62bcad30d
Fernando Isnaldo Silva de Faria <fernando-i@outlook.com>
2026-09-12 22:13:25 -0300

    Update GeradordeSenhas.java

--- GeradordeSenhas.java
+++ GeradordeSenhas.java
@@ -49,7 +49,7 @@ public class GeradordeSenhas {
                              AVISO DE CONTEÚDO:
                              O modo sílaba utiliza amostragem combinatória puramente aleatória (C + V + T).
                              Em casos raros (~0,09%), o programa pode gerar sílabas que coincidem com palavras ofensivas ou inapropriadas em múltiplos idiomas.
-                             Não há filtros de exclusão para preservar a integridade estrita da entropia criptográfica
+                             Não há filtros de exclusão para preservar a integridade estrita da entropia criptográfica.
 
                              Exemplos de uso:
 
